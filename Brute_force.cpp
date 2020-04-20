650fa24c3297f5928ec914324a95ea9ffbe16ddd
Mateusz Urbaniak <241614@student.pwr.edu.pl>
2020-04-20 08:34:52 +0200

    Deleting one unnecessary function

--- Brute_force.cpp
+++ Brute_force.cpp
@@ -12,22 +12,10 @@ int opt_permutation(std::vector<parameters> dane) {
 	} while (next_permutation(dane.begin(), dane.end(), compare_ID));
 	return best_F;
 }
-
-bool shouldSwap(std::vector<parameters> str, int start, int curr) {
-	for (int i = start; i < curr; i++) {
-		//if (str[i].p == str[curr].p && str[i].w == str[curr].w && str[i].d == str[curr].d)
-		if (str[i].ID == str[curr].ID)
-			return false;
-	}
-	return true;
-}
  int best_F = INT16_MAX;
  int temp;
 void findPermutations(std::vector<parameters> str, int index, size_t n) {
 	if (index >= n) {
-		//cout << str[0].ID <<" " << str[1].ID <<" " << str[2].ID << endl;
-		//cout << zmienna++ << endl;
-		//print(str);
 		temp = calculate_F(str);
 		if (temp < best_F) {
 			best_F = temp;
@@ -35,14 +23,9 @@ void findPermutations(std::vector<parameters> str, int index, size_t n) {
 		return;
 	}
 	for (int i = index; i < n; i++) {
-		// Proceed further for str[i] only if it  
-		// doesn't match with any of the characters 
-		// after str[index] 
-		//bool check = shouldSwap(str, index, i);
-		//if (check) {
 		std::swap(str[index], str[i]);
 		findPermutations(str, index + 1, n);
 		std::swap(str[index], str[i]);
-		//}
+		
 	}
 }
--- Brute_force.h
+++ Brute_force.h
@@ -2,5 +2,4 @@
 #include "SortD.h"
 #include <algorithm>
 int opt_permutation(std::vector<parameters> dane);
-bool shouldSwap(std::vector<parameters> str, int start, int curr);
 void findPermutations(std::vector<parameters> str, int index, size_t n);
